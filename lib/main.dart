514506cabc442fc80aa083f1c0f78c3430c74ec5
bharathisukumar99@gmail.com <bharathi@millionitechnology.com>
2024-03-02 14:19:59 +0530

    login setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 import 'product_model.dart';
@@ -8,14 +9,112 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  State<MainApp> createState() => _MainAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
 }
 
-class _MainAppState extends State<MainApp> {
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Card(
+            child: ListTile(
+              title: const Text('Login Page'),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()));
+              },
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: const Text('Product'),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProductPage()));
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var username = TextEditingController();
+  var password = TextEditingController();
+  var loading = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: username,
+            ),
+            TextField(
+              controller: password,
+            ),
+            FilledButton(
+                onPressed: () async {
+                  setState(() {
+                    loading = true;
+                  });
+                  if (username.text.isNotEmpty && password.text.isNotEmpty) {
+                    var response = await login(username.text, password.text);
+                    if (response == true) {
+                      //navigation
+                    }
+                  }
+
+                  setState(() {
+                    loading = false;
+                  });
+                },
+                child: const Text('Login')),
+            Visibility(
+                visible: loading, child: const CircularProgressIndicator())
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProductPage extends StatefulWidget {
+  const ProductPage({super.key});
+
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
   late Future<List<Product>> futureProduct;
   @override
   void initState() {
@@ -26,43 +125,41 @@ class _MainAppState extends State<MainApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          body: FutureBuilder(
-        future: futureProduct,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            var data = snapshot.data;
-            return ListView.builder(
-              itemCount: data!.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Column(
-                    children: [
-                      FadeInImage(
-                        width: double.infinity,
-                        height: 400,
-                        placeholder: MemoryImage(kTransparentImage),
-                        image: NetworkImage(data[index].image),
+    return Scaffold(
+        body: FutureBuilder(
+      future: futureProduct,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          var data = snapshot.data;
+          return ListView.builder(
+            itemCount: data!.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Column(
+                  children: [
+                    FadeInImage(
+                      width: double.infinity,
+                      height: 400,
+                      placeholder: MemoryImage(kTransparentImage),
+                      image: NetworkImage(data[index].image),
+                    ),
+                    ListTile(
+                      title: Text(
+                        data[index].title,
+                        style: const TextStyle(fontWeight: FontWeight.w900),
                       ),
-                      ListTile(
-                        title: Text(
-                          data[index].title,
-                          style: const TextStyle(fontWeight: FontWeight.w900),
-                        ),
-                        subtitle: Text(data[index].description),
-                        trailing: Text('\$${data[index].price.toString()}'),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            );
-          }
-          return const Center(child: CircularProgressIndicator());
-        },
-      )),
-    );
+                      subtitle: Text(data[index].description),
+                      trailing: Text('\$${data[index].price.toString()}'),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        }
+        return const Center(child: CircularProgressIndicator());
+      },
+    ));
   }
 }
 
--- lib/service.dart
+++ lib/service.dart
@@ -20,3 +20,24 @@ Future<List<Product>> fetchProduct() async {
 
   return [];
 }
+
+Future<bool> login(String username, String password) async {
+  try {
+    var response = await http.post(
+        Uri.parse("http://staging.challengexpress.in/api/v1/authenticate"),
+        headers: {
+          'accept': 'application/json',
+          'Content-Type': 'application/json-patch+json',
+          'Authorization':
+              'Basic ZnJVd3JVZ3V6b21sYnJBWDpmcmVGYXN3b3N0dXJ1U1RsVk9qaVl1dGl3cnVicmx5SQ=='
+        },
+        body: jsonEncode({"userName": username, "password": password}));
+
+    if (response.statusCode == 200) {
+      return true;
+    }
+  } catch (e) {
+    return false;
+  }
+  return false;
+}
